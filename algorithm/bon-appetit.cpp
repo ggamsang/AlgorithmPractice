3c6663517c90426f2d16b40f30b04aa2aa886875
GGamSang <w.ggmsng@gmail.com>
2018-02-27 13:08:45 +0900

    bon-appetit!

---
+++ algorithm/bon-appetit.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main(){
+  int n = 0, k = 0, b = 0, c[100000] = {0,};
+  
+  std::cin >> n >> k;
+
+  for ( int i=0; i<n; i++)
+    std::cin >> c[i];
+  std::cin >> b;
+
+  int total = 0;
+  for ( int i=0; i<n; i++)
+    if(i != k)
+      total += c[i];
+
+  if(total/2 == b) std::cout << "Bon Appetit" << std::endl;
+  else std::cout << b - (total / 2) << std::endl;
+
+  return 0;
+}
