ed8a938b437970c15617bbde63d8912ba9c56df4
GGamSang <w.ggmsng@gmail.com>
2018-03-11 15:13:18 +0900

    think of '0' case.

---
+++ algorithm/easy_save-the-prisoner.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+
+int prisoner(int n, int m, int s) {
+  return (m%n+s-1)%n==0?n:(m%n+s-1)%n;
+}
+int main(int argc, char* argv[]) {
+  int t, n, m, s;
+  std::cin >> t;
+  for(int i=0; i<t; ++i)
+    {
+      std::cin >> n >> m >> s;
+      std::cout << prisoner(n, m, s) <<std::endl;
+    }
+  return 0;
+}
