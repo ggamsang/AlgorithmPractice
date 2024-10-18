344ebbbc30ae2a99e29ca91d7c9315daaeffb398
ggamsang <w.ggmsng@gmail.com>
2024-10-18 12:33:01 +0900

    update: 2623 메모이즈 코드 수정

--- leetcode/2623.Memoize.ts
+++ leetcode/2623.Memoize.ts
@@ -1,2 +1,19 @@
 type Fn = (...params: number[]) => number
 
+function memoize(fn: Fn): Fn {
+  let callCount: number = 0
+  const memoize: Map<string, number> = new Map()
+
+  return function (...args) {
+    const key = JSON.stringify(args)
+    let value: number | undefined = memoize.get(key) 
+
+    if (value === undefined) { /* prefer to use NULL*/
+      value = fn(...args)
+      memoize.set(key, value)
+      callCount++
+    }
+
+    return value
+  }
+}
