c7857521a587d1222f4e05d8b6d322d6ce1556c3
ggamsang <w.ggmsng@gmail.com>
2024-11-13 13:54:02 +0900

    fix: 리트코드_242애나그램검증_코드수정

--- leetcode/242.ValidAnagram.ts
+++ leetcode/242.ValidAnagram.ts
@@ -1,33 +1,31 @@
 function getAlphabetBins(input: string): number[] {
-  return input
-    .toLowerCase()
-    .split("")
-    .reduce((acc: number[], char: string) => {
-      if (char >= "a" && char <= "z") {
-        const index = char.charCodeAt(0) - "a".charCodeAt(0);
-        acc[index]++;
-      }
-      return acc;
-    }, new Array(26).fill(0));
+  const alphabets= "abcdefghijklmnopqrstuvwxyz"
+  const bin:number[]= new Array(alphabets.length).fill(0)
+  
+  for (let i = 0; i < input.length; i++) {
+    bin[alphabets.indexOf(input[i])]++
+  }
+  
+  return bin
 }
 
 function compare(arr1: number[], arr2: number[]): boolean {
   if (arr1.length !== arr2.length) {
-    return false;
+    return false
   }
 
   for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] !== arr2[i]) {
-      return false;
+      return false
     }
   }
 
-  return true;
-  //return arr1.every((value, index) => value === arr2[index]);
+  return true
 }
 
 function isAnagram(s: string, t: string): boolean {
-  const binS = getAlphabetBins(s);
-  const binT = getAlphabetBins(t);
-  return compare(binS, binT);
+  const binS = getAlphabetBins(s)
+  const binT = getAlphabetBins(t)
+
+  return compare(binS, binT)
 }
